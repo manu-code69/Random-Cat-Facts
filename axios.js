acd2bbbc5d861b68da18e3708ffd1dbf8e507a46
manu-code69 <kathpalmohak21@gmail.com>
2026-07-13 21:20:18 +0530

    Refactor Axios usage and event handling
    Refactor code to improve readability and functionality, including event listener fixes and better structure for displaying facts and images.

--- axios.js
+++ axios.js
@@ -5,7 +5,7 @@
 
 // From Fetch we have to convert first in usble data ;
 
-let url = "https://catfact.ninja/fact";
+let url = "https://catfact.ninja/fact";                 // CAT API
 
 async function start(){
     try{
@@ -13,29 +13,57 @@ async function start(){
     return res.data.fact
     }
     catch(e){
-        return "No Fact Found";
+        return "No Facts Found"
     }
 
 }
 
-let btn = document.querySelector("button")
+let btn = document.querySelector("#o")
 
-addEventListener("click",display_fact);
-
+btn.addEventListener("click",display_fact);
 
 // display in html
 
+let h1 = document.querySelector("h1")
+
+let fact = document.createElement("h4")
+
+h1.append(fact)
+
 async function display_fact(){
-
+   
     let random_fact = await start();
-
-    let h1 = document.querySelector("h1")
-
-    let fact = document.createElement("h4")
     fact.innerHTML = random_fact
-    h1.append(fact)
+    
+    // let hr = document.createElement("hr")
+    // h1.append(hr);
+}
 
-    let hr = document.createElement("hr")
-    h1.append(hr);
+// ----------------  DOG_img  ---------------------------
+
+let url2 = "https://dog.ceo/api/breeds/image/random";   // DOG API
+
+let btn2 = document.querySelector("#t")
+
+async function start2(){
+    try{
+        let res = await axios.get(url2)
+        return res.data.message
+    }
+    catch{
+        return "Could Not Load Image";
+    }
+}
+
+btn2.addEventListener("click",display_2)
+
+async function display_2(){
+
+    let div = document.querySelector("div")
+    div.innerHTML = "";                 // At Every Call Set Div = ""  So only 1 img shown at a time
+    let dog_img = await start2()
+    let img = document.createElement("img")
+    img.src = dog_img
+    div.insertAdjacentElement("afterbegin",img)
 
 }
